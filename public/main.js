f680f19f6bcbe8aa4b688d9395f75d1f036b90d0
Nauman_Asghar <noumanasghar272@gmail.com>
2025-01-28 02:14:45 +0500

    added customer report and related functionality

--- public/main.js
+++ public/main.js
@@ -10,3 +10,7 @@ document.querySelector("#addArticle").addEventListener("click",function (event){
     event.preventDefault();
     window.location.href="productForm.html";
 });
+document.getElementById('view-customers').addEventListener('click', function (event){
+    event.preventDefault();
+    window.location.href = "customer.html"; // Adjust path as needed
+  });
