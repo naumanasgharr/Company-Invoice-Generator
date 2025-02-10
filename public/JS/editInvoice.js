7fa7d984dc1807695aa91abce1a81e9853c06dcd
Nauman_Asghar <noumanasghar272@gmail.com>
2025-02-10 08:56:08 +0500

    add edit button | complete generating data

--- public/JS/editInvoice.js
+++ public/JS/editInvoice.js
@@ -11,12 +11,16 @@ fetch("http://localhost:3000/api/selectInvoice")
         });
         select.addEventListener("change",function (){
             const selectedInvoice = this.value;
+            const mainDiv = document.querySelector('.form');
+            mainDiv.innerHTML = '';
             if(selectedInvoice != 'select'){
                 fetch(`http://localhost:3000/api/invoiceDetails?invoice_number=${selectedInvoice}`)
                     .then(response=>response.json())
                     .then(data=>{
                         const invoiceData = data.invoiceData;
-                        console.log(invoiceData);
+                        const orderData = data.orders;
+                       // console.log(orderData);
+                      //  console.log(invoiceData);
                        // console.log(orderData);
                         const editForm = document.createElement('form');
                         editForm.name = 'editForm';
@@ -56,13 +60,72 @@ fetch("http://localhost:3000/api/selectInvoice")
                         customerIDInput.id = 'customerID';
                         customerIDInput.type = 'text';
                         customerIDInput.value = `${invoiceData.customer_id}`;
-
-                        // appending labels , inputs, and div
                         invoiceDiv.append(orderDateLabel,orderDateInput,invoiceNumberLabel,invoiceNumberInput,customerIDLabel,customerIDInput);
-                        editForm.appendChild(invoiceDiv);
-                        // document.body.appendChild(editForm);
                         
-                        const mainDiv = document.querySelector('.form');
+                         orderData.forEach(order=>{
+                            const orderDetails = order.orderDetails;
+                            const productRow = document.createElement('div');
+                            productRow.className = 'productRow';
+                            orderDetails.forEach(orderDetail=>{
+                                const productNumberLabel = document.createElement('label');
+                                productNumberLabel.htmlFor = 'productNumber';
+                                productNumberLabel.innerText = 'Article ID: ';
+                                const productNumberInput = document.createElement('input');
+                                productNumberInput.name = 'productNumber';
+                                productNumberInput.id = 'productNumber';
+                                productNumberInput.type = 'text';
+                                productNumberInput.value = `${orderDetail.article_number}`;
+
+                                const productAmountLabel = document.createElement('label');
+                                productAmountLabel.htmlFor = 'productAmount';
+                                productAmountLabel.innerText = 'Article Amount: ';
+                                const productAmountInput = document.createElement('input');
+                                productAmountInput.name = 'productAmount';
+                                productAmountInput.id = 'productAmount';
+                                productAmountInput.type = 'text';
+                                productAmountInput.value = `${orderDetail.article_amount}`;
+
+                                const unitPriceLabel = document.createElement('label');
+                                unitPriceLabel.htmlFor = 'unitPrice';
+                                unitPriceLabel.innerText = 'Unit Price : ';
+                                const unitPriceInput = document.createElement('input');
+                                unitPriceInput.name = 'unitPrice';
+                                unitPriceInput.id = 'unitPrice';
+                                unitPriceInput.type = 'number';
+                                unitPriceInput.min = '0';
+                                unitPriceInput.step = '0.01';
+                                unitPriceInput.value = `${orderDetail.unit_price}`;
+
+                                const currencyLabel = document.createElement('label');
+                                currencyLabel.htmlFor = 'currency';
+                                currencyLabel.innerText = 'Currency: ';
+                                const currencySelect = document.createElement('select');
+                                currencySelect.name = 'currency';
+                                currencySelect.className = 'currency';
+                                ['USD', 'PKR', 'SAUDI RIYAL', 'UAE DHIRAM'].forEach(currency => {
+                                    const option = document.createElement('option');
+                                    option.innerText = currency;
+                                    currencySelect.appendChild(option);
+                                });
+
+                                const orderNumberLabel = document.createElement('label');
+                                orderNumberLabel.htmlFor = 'orderNumber';
+                                orderNumberLabel.innerText = 'PO Number: ';
+                                const orderNumberInput = document.createElement('input');
+                                orderNumberInput.name = 'orderNumber';
+                                orderNumberInput.id = 'orderNumber';
+                                orderNumberInput.type = 'text';
+                                orderNumberInput.value = `${orderDetail.order_number}`;
+
+                                productRow.append(productNumberLabel,productNumberInput,productAmountLabel,productAmountInput,unitPriceLabel,unitPriceInput,currencyLabel,currencySelect,orderNumberLabel,orderNumberInput);
+                                productDiv.appendChild(productRow);
+                            });
+                            
+                         });
+
+
+
+                        editForm.appendChild(invoiceDiv);
                         editForm.appendChild(productDiv); 
                         mainDiv.appendChild(editForm);
 
